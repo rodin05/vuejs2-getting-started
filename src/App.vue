8fc5f16ec46d577dc0b51b148347f1b02193894a
rodin05 <694463215@qq.com>
2017-08-07 00:39:14 +0800

    added table.html

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app">
-    <!--<h1>Let's go somewhere!</h1>-->
-    <!--<router-link to="/infiniteLoad">to infiniteLoad</router-link>-->
-    <!--<hr>-->
-    <!--<router-link to="/tables">to tables</router-link>-->
-    <!--<hr>-->
-    <!--<router-link to="/mintuiexa">to mintuiexa</router-link>-->
-    <!--<hr>-->
+    <h1>Let's go somewhere!</h1>
+    <router-link to="/infiniteLoad">to infiniteLoad</router-link>
+    <hr>
+    <router-link to="/tables">to tables</router-link>
+    <hr>
+    <router-link to="/mintuiexa">to mintuiexa</router-link>
+    <hr>
+    <router-link to="/datatabless">to datatabless</router-link>
+    <hr>
     <router-view></router-view>
   </div>
 </template>
